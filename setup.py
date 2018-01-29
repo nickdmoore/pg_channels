72a36934f9cf0e19abbcf412c911b13f0864426c
Nick <nick@nickdata.com>
2018-01-28 22:30:18 -0800

    Bumped version and udpated setup.py for PyPI

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@ from setuptools import setup
 setup(
 	name = 'pg_channels',
 	packages = ['pg_channels'],
-	version = '0.1',
+	version = '1.0',
 	license='MIT',
 	description = 'Python wrapper for PostgreSQL NOTIFY and LISTEN commands.',
 	author = 'Nick Moore',
 	author_email = 'nickdmoore@gmail.com',
 	url = 'https://github.com/nickdmoore/pg_channels',
+	download_url = 'https://github.com/nickdmoore/pg_channels/archive/1.0.tar.gz',
 	keywords = ['PostgreSQL', 'NOTIFY', 'LISTEN', 'channels'],
 	install_requires=[
 		'psycopg2>=2.7.3.2'
 	],
 	classifiers=[
-		'Development Status :: 4 - Beta',
 		'Programming Language :: Python',
 		'Programming Language :: Python :: 3.6'
 	]
